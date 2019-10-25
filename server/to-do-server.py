6f0666dba49afb2df69e89b6a746a05aec7d12f0
***** *** <rohroexperiment@rohroexperiment.com>
2019-10-25 17:49:39 +0530

    move node to server

---
+++ server/to-do-server.py
@@ -0,0 +1,71 @@
+from flask import Flask, redirect, request
+import json
+
+app = Flask(__name__)
+
+database = {'YouTube':[]}
+
+
+@app.route('/')
+def goto():
+    return redirect('static/to-do.html', 301)
+
+
+@app.route('/todolists/', methods=['GET', 'POST', 'DELETE'])
+def hello_world():
+    data = request.data.decode()
+    if request.method == 'POST':
+        if data not in database.keys():
+            database[data] = []
+    if request.method == 'DELETE':
+        if data in database.keys():
+            del database[data]
+    return json.dumps(list(database.keys()))
+
+
+@app.route('/todolists/<name>/tasks', methods=['GET', 'POST'])
+def gettodotilts(name):
+    data = request.data.decode()
+    if request.method == 'POST':
+        data = json.loads(data)
+        if data['text'] != '':
+            database[name].append(data)
+    return json.dumps(database[name])
+
+
+@app.route('/todolists/<name>/cct', methods=['POST'])
+def cct(name):
+    data = []
+    for i in database[name]:
+        if not i['checked']:
+            data.append(i)
+    database[name] = data
+    return json.dumps(database[name])
+
+
+@app.route('/todolists/<name>/tasks/count/')
+def gettaskcount(name):
+    nonchecked = 0
+    for i in database[name]:
+        if not i['checked']:
+            nonchecked += 1
+    return json.dumps({'total': len(database[name]), 'todo': nonchecked})
+
+
+@app.route('/todolists/<name>/<id>/toggleChecked/', methods=['POST'])
+def toggleChecked(name, id):
+    stat = 200
+    try:
+        id = int(id)
+        database[name][id]['checked'] = not database[name][id]['checked']
+    except ValueError:
+        stat = 400
+    nonchecked = 0
+    for i in database[name]:
+        if not i['checked']:
+            nonchecked += 1
+    return json.dumps({'total': len(database[name]), 'todo': nonchecked}),stat
+
+
+if __name__ == '__main__':
+    app.run(port=3000)
