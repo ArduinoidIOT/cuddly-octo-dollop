c3a8fa6a9902aaca3fc2fe3feccf7924d3abe5c4
***** *** <rohroexperiment@rohroexperiment.com>
2019-10-27 13:39:19 +0530

    add max length

--- server/to-do-server.py
+++ server/to-do-server.py
@@ -4,7 +4,7 @@ import json
 
 app = Flask(__name__)
 
-database = {'YouTube':[]}
+database = {"defaultUser": {'YouTube':[]}}
 
 
 @app.route('/')
@@ -16,12 +16,12 @@ def goto():
 def hello_world():
     data = request.data.decode()
     if request.method == 'POST':
-        if data not in database.keys():
-            database[data] = []
+        if data not in database['defaultUser'].keys() and len(data) < 12:
+            database['defaultUser'][data] = []
     if request.method == 'DELETE':
-        if data in database.keys():
-            del database[data]
-    return json.dumps(list(database.keys()))
+        if data in database['defaultUser'].keys():
+            del database['defaultUser'][data]
+    return json.dumps(list(database['defaultUser'].keys()))
 
 
 @app.route('/todolists/<name>/tasks', methods=['GET', 'POST'])
@@ -30,27 +30,27 @@ def gettodotilts(name):
     if request.method == 'POST':
         data = json.loads(data)
         if data['text'] != '':
-            database[name].append(data)
-    return json.dumps(database[name])
+            database['defaultUser'][name].append(data)
+    return json.dumps(database['defaultUser'][name])
 
 
 @app.route('/todolists/<name>/cct', methods=['POST'])
 def cct(name):
     data = []
-    for i in database[name]:
+    for i in database['defaultUser'][name]:
         if not i['checked']:
             data.append(i)
-    database[name] = data
-    return json.dumps(database[name])
+    database['defaultUser'][name] = data
+    return json.dumps(database['defaultUser'][name])
 
 
 @app.route('/todolists/<name>/tasks/count/')
 def gettaskcount(name):
     nonchecked = 0
-    for i in database[name]:
+    for i in database['defaultUser'][name]:
         if not i['checked']:
             nonchecked += 1
-    return json.dumps({'total': len(database[name]), 'todo': nonchecked})
+    return json.dumps({'total': len(database['defaultUser'][name]), 'todo': nonchecked})
 
 
 @app.route('/todolists/<name>/<id>/toggleChecked/', methods=['POST'])
@@ -58,14 +58,14 @@ def toggleChecked(name, id):
     stat = 200
     try:
         id = int(id)
-        database[name][id]['checked'] = not database[name][id]['checked']
+        database['defaultUser'][name][id]['checked'] = not database['defaultUser'][name][id]['checked']
     except ValueError:
         stat = 400
     nonchecked = 0
-    for i in database[name]:
+    for i in database['defaultUser'][name]:
         if not i['checked']:
             nonchecked += 1
-    return json.dumps({'total': len(database[name]), 'todo': nonchecked}),stat
+    return json.dumps({'total': len(database['defaultUser'][name]), 'todo': nonchecked}),stat
 
 
 if __name__ == '__main__':
